9d77eee5d7fa5a20bb97e946fc8e4128617f0fd4
garrettstankoven <gstankoven@gmail.com>
2022-05-14 12:23:28 -0700

    Initial commit for the paint application

--- README.md
+++
@@ -1,2 +0,0 @@
-# paint_app
-An application showing the available stock of paint for a painting company.
---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Paint Availability</h1>
+
+      
+    </div>
+  );
+}
+
+export default App;
